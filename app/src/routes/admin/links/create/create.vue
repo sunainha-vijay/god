795b2c7206dde8d7fb15d9ec019b213e5172c087
Sunainha Vijay <113001688+sunainha-vijay@users.noreply.github.com>
2024-07-11 17:29:45 +0530

    Update create.vue

--- app/src/routes/admin/links/create/create.vue
+++ app/src/routes/admin/links/create/create.vue
@@ -1,69 +1,69 @@
 <template>
-	<admin-view>
-		<h1>Create Link</h1>
-		<n-spin :show="showLoadingSpinner">
-			<n-form ref="formRef" class="centered-form" :model="model" :rules="rules">
-				<n-form-item path="url" label="URL">
-					<n-input
-						v-model:value="model.url_raw"
-						class="url-input"
-						pair
-						clearable
-						separator="://"
-						:placeholder="['Protocol', 'Web Address']"
-						@change="handleUrlUpdate"
-						@update:value="handleUrlUpdate"
-					></n-input>
-				</n-form-item>
-				<n-row>
-					<n-form-item ref="slugRef" path="slug" label="Slug" style="flex-grow: 1">
-						<n-input-group>
-							<n-input-group-label class="slug-input-inline">/</n-input-group-label>
-							<n-input v-model:value="model.slug" class="slug-input" placeholder="Enter Slug" />
-						</n-input-group>
-					</n-form-item>
-					<n-form-item>
-						<n-button type="warning" style="margin-left: 20px" @click="handleGenerateSlug">
-							<template #icon>
-								<n-icon>
-									<sync />
-								</n-icon>
-							</template>
-							Generate Slug
-						</n-button>
-					</n-form-item>
-				</n-row>
-				<n-row>
-					<n-form-item path="start_date" label="Start Date">
-						<n-date-picker
-							v-model:value="model.start_date"
-							type="datetime"
-							placeholder="Select Start Date"
-							format="yyyy-MM-dd HH:mm:ss"
-						/>
-					</n-form-item>
-					<n-form-item path="end_date" label="End Date" style="margin-left: 20px">
-						<n-date-picker
-							v-model:value="model.end_date"
-							type="datetime"
-							placeholder="Select End Date"
-							format="yyyy-MM-dd HH:mm:ss"
-						/>
-					</n-form-item>
-				</n-row>
-				<div style="display: flex; justify-content: center">
-					<n-button round type="primary" :disabled="showLoadingSpinner" @click="handleCreateLink">
-						<template #icon>
-							<n-icon>
-								<plus />
-							</n-icon>
-						</template>
-						Create
-					</n-button>
-				</div>
-			</n-form>
-		</n-spin>
-	</admin-view>
+  <admin-view>
+    <h1>Create Link</h1>
+    <n-spin :show="showLoadingSpinner">
+      <n-form ref="formRef" class="centered-form" :model="model" :rules="rules">
+        <n-form-item path="url" label="URL">
+          <n-input
+            v-model:value="model.url_raw"
+            class="url-input"
+            pair
+            clearable
+            separator="://"
+            :placeholder="['Protocol', 'Web Address']"
+            @change="handleUrlUpdate"
+            @update:value="handleUrlUpdate"
+          ></n-input>
+        </n-form-item>
+        <n-row>
+          <n-form-item ref="slugRef" path="slug" label="Slug" style="flex-grow: 1">
+            <n-input-group>
+              <n-input-group-label class="slug-input-inline">/</n-input-group-label>
+              <n-input v-model:value="model.slug" class="slug-input" placeholder="Enter Slug" />
+            </n-input-group>
+          </n-form-item>
+          <n-form-item>
+            <n-button type="warning" style="margin-left: 20px" @click="handleGenerateSlug">
+              <template #icon>
+                <n-icon>
+                  <sync />
+                </n-icon>
+              </template>
+              Generate Slug
+            </n-button>
+          </n-form-item>
+        </n-row>
+        <n-row>
+          <n-form-item path="start_date" label="Start Date">
+            <n-date-picker
+              v-model:value="model.start_date"
+              type="datetime"
+              placeholder="Select Start Date"
+              format="yyyy-MM-dd HH:mm:ss"
+            />
+          </n-form-item>
+          <n-form-item path="end_date" label="End Date" style="margin-left: 20px">
+            <n-date-picker
+              v-model:value="model.end_date"
+              type="datetime"
+              placeholder="Select End Date"
+              format="yyyy-MM-dd HH:mm:ss"
+            />
+          </n-form-item>
+        </n-row>
+        <div style="display: flex; justify-content: center">
+          <n-button round type="primary" :disabled="showLoadingSpinner" @click="handleCreateLink">
+            <template #icon>
+              <n-icon>
+                <plus />
+              </n-icon>
+            </template>
+            Create
+          </n-button>
+        </div>
+      </n-form>
+    </n-spin>
+  </admin-view>
 </template>
 
 <script lang="ts">
@@ -72,193 +72,193 @@ import { addLink } from '@/services/links';
 import { useAppStore } from '@/stores/appStore';
 import { useLinksStore } from '@/stores/linksStore';
 import {
-	useMessage,
-	NSpin,
-	NForm,
-	NFormItem,
-	NInput,
-	NInputGroup,
-	NInputGroupLabel,
-	NDatePicker,
-	NIcon,
-	NButton,
-	NRow,
+  useMessage,
+  NSpin,
+  NForm,
+  NFormItem,
+  NInput,
+  NInputGroup,
+  NInputGroupLabel,
+  NDatePicker,
+  NIcon,
+  NButton,
+  NRow,
 } from 'naive-ui';
 import { Plus, Sync } from '@vicons/fa';
 import { customAlphabet } from 'nanoid';
 
 export default defineComponent({
-	components: { NSpin, NForm, NFormItem, NInput, NInputGroup, NInputGroupLabel, NDatePicker, NIcon, NButton, NRow, Plus, Sync },
-	setup() {
-		const showLoadingSpinner = ref(false);
-		const formRef = ref<any>(null);
-		const slugRef = ref<any>(null);
-		const messageDuration = 5000;
-		const appStore = useAppStore();
-		const linksStore = useLinksStore();
-		const message = useMessage();
-		
-		const now = new Date();
-		const defaultEndDate = new Date(now.getTime() + 24 * 60 * 60 * 1000);
+  components: { NSpin, NForm, NFormItem, NInput, NInputGroup, NInputGroupLabel, NDatePicker, NIcon, NButton, NRow, Plus, Sync },
+  setup() {
+    const showLoadingSpinner = ref(false);
+    const formRef = ref<any>(null);
+    const slugRef = ref<any>(null);
+    const messageDuration = 5000;
+    const appStore = useAppStore();
+    const linksStore = useLinksStore();
+    const message = useMessage();
 
-		const modelRef = ref({
-			url_raw: ['', ''] as [string, string],
-			slug: '',
-			start_date: now,
-			end_date: defaultEndDate,
-		});
+    const now = new Date();
+    const defaultEndDate = new Date(now.getTime() + 24 * 60 * 60 * 1000);
 
-		const rules = {
-			url: [
-				{
-					required: true,
-					validator(rule: any, value: any) {
-						if (!value) {
-							return new Error('URL is required');
-						} else if (value.length > 2083) {
-							return new Error('URL has to be 2083 characters or below.');
-						} else if (String(value).startsWith('://')) {
-							return new Error('Please enter a protocol.');
-						}
-						return true;
-					},
-					trigger: ['input', 'blur'],
-				},
-			],
-			slug: [
-				{
-					required: true,
-					validator(rule: any, value: any) {
-						if (!value) {
-							return new Error('Slug is required');
-						}
-						if (value.length > 50) {
-							return new Error('Slug has to be 50 characters or below.');
-						}
-						return true;
-					},
-					trigger: ['input', 'blur'],
-				},
-			],
-			start_date: [
-				{
-					required: true,
-					validator(rule: any, value: any) {
-						if (!value) {
-							return new Error('Start date is required');
-						}
-						return true;
-					},
-					trigger: ['change', 'blur'],
-				},
-			],
-			end_date: [
-				{
-					required: true,
-					validator(rule: any, value: any) {
-						if (!value) {
-							return new Error('End date is required');
-						}
-						return true;
-					},
-					trigger: ['change', 'blur'],
-				},
-			],
-		};
+    const modelRef = ref({
+      url_raw: ['', ''] as [string, string],
+      slug: '',
+      start_date: now.toISOString(), // Convert Date to ISO string
+      end_date: defaultEndDate.toISOString(), // Convert Date to ISO string
+    });
 
-		// Remove confusion with caps I caps O and l
-		const nanoid = customAlphabet('1234567890abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ', 6);
+    const rules = {
+      url: [
+        {
+          required: true,
+          validator(rule: any, value: any) {
+            if (!value) {
+              return new Error('URL is required');
+            } else if (value.length > 2083) {
+              return new Error('URL has to be 2083 characters or below.');
+            } else if (String(value).startsWith('://')) {
+              return new Error('Please enter a protocol.');
+            }
+            return true;
+          },
+          trigger: ['input', 'blur'],
+        },
+      ],
+      slug: [
+        {
+          required: true,
+          validator(rule: any, value: any) {
+            if (!value) {
+              return new Error('Slug isrequired');
+            }
+            if (value.length > 50) {
+              return new Error('Slug has to be 50 characters or below.');
+            }
+            return true;
+          },
+          trigger: ['input', 'blur'],
+        },
+      ],
+      start_date: [
+        {
+          required: true,
+          validator(rule: any, value: any) {
+            if (!value) {
+              return new Error('Start date is required');
+            }
+            return true;
+          },
+          trigger: ['change', 'blur'],
+        },
+      ],
+      end_date: [
+        {
+          required: true,
+          validator(rule: any, value: any) {
+            if (!value) {
+              return new Error('End date is required');
+            }
+            return true;
+          },
+          trigger: ['change', 'blur'],
+        },
+      ],
+    };
 
-		async function handleGenerateSlug() {
-			modelRef.value.slug = nanoid();
-			try {
-				await slugRef.value.validate();
-			} catch (error) {
-				return;
-			}
-		}
+    // Remove confusion with caps I caps O and l
+    const nanoid = customAlphabet('1234567890abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ', 6);
 
-		async function handleCreateLink() {
-			try {
-				await formRef.value.validate();
-			} catch (error) {
-				return;
-			}
-			try {
-				showLoadingSpinner.value = true;
-				const { data, error } = await addLink({
-					user_id: appStore.supabaseSession!.user!.id,
-					url: modelRef.value.url_raw.join('://'),
-					slug: modelRef.value.slug,
-					start_date: modelRef.value.start_date.toISOString(),
-					end_date: modelRef.value.end_date.toISOString(),
-				});
-				if (error) throw error;
+    async function handleGenerateSlug() {
+      modelRef.value.slug = nanoid();
+      try {
+        await slugRef.value.validate();
+      } catch (error) {
+        return;
+      }
+    }
 
-				linksStore.addLink(data);
-				resetForm();
-				message.success('Link successfully created!', { duration: messageDuration });
-			} catch (error: any) {
-				if (error.code == '23505') {
-					message.error('Slug already exists. Please change the slug.', { duration: messageDuration });
-				} else {
-					message.error('Error creating link...', { duration: messageDuration });
-				}
-			} finally {
-				showLoadingSpinner.value = false;
-			}
-		}
+    async function handleCreateLink() {
+      try {
+        await formRef.value.validate();
+      } catch (error) {
+        return;
+      }
+      try {
+        showLoadingSpinner.value = true;
+        const { data, error } = await addLink({
+          user_id: appStore.supabaseSession!.user!.id,
+          url: modelRef.value.url_raw.join('://'),
+          slug: modelRef.value.slug,
+          start_date: modelRef.value.start_date,
+          end_date: modelRef.value.end_date,
+        });
+        if (error) throw error;
 
-		function resetForm() {
-			modelRef.value.url_raw = ['', ''];
-			modelRef.value.slug = '';
-			modelRef.value.start_date = new Date();
-			modelRef.value.end_date = new Date(modelRef.value.start_date.getTime() + 24 * 60 * 60 * 1000);
-		}
+        linksStore.addLink(data);
+        resetForm();
+        message.success('Link successfully created!', { duration: messageDuration });
+      } catch (error: any) {
+        if (error.code == '23505') {
+          message.error('Slug already exists. Please change the slug.', { duration: messageDuration });
+        } else {
+          message.error('Error creating link...', { duration: messageDuration });
+        }
+      } finally {
+        showLoadingSpinner.value = false;
+      }
+    }
 
-		function handleUrlUpdate(val: [string, string]) {
-			modelRef.value.url_raw = val;
-		}
+    function resetForm() {
+      modelRef.value.url_raw = ['', ''];
+      modelRef.value.slug = '';
+      modelRef.value.start_date = now.toISOString();
+      modelRef.value.end_date = new Date(modelRef.value.start_date.getTime() + 24 * 60 * 60 * 1000).toISOString();
+    }
 
-		return {
-			showLoadingSpinner,
-			formRef,
-			slugRef,
-			model: modelRef,
-			rules,
-			handleGenerateSlug,
-			handleCreateLink,
-			handleUrlUpdate,
-		};
-	},
+    function handleUrlUpdate(val: [string, string]) {
+      modelRef.value.url_raw = val;
+    }
+
+    return {
+      showLoadingSpinner,
+      formRef,
+      slugRef,
+      model: modelRef,
+      rules,
+      handleGenerateSlug,
+      handleCreateLink,
+      handleUrlUpdate,
+    };
+  },
 });
 </script>
 
 <style scoped>
 .centered-form {
-	width: 500px;
-	margin: 0 auto;
+  width: 500px;
+  margin: 0 auto;
 }
 
 .slug-input {
-	text-align: center;
+  text-align: center;
 }
 
 .slug-input-inline {
-	width: 33%;
-	text-align: right;
+  width: 33%;
+  text-align: right;
 }
 
 .url-input :deep(.n-input-wrapper):first-child {
-	flex-grow: 0;
-	width: 80px;
+  flex-grow: 0;
+  width: 80px;
 }
 
 .url-input :deep(.n-input-wrapper):first-child input {
-	text-align: right;
+  text-align: right;
 }
 
 .url-input :deep(.n-input-wrapper):nth-child(3) input {
-	text-align: left;
+  text-align: left;
 }
 </style>
